5bdd7bfc40c77b9dc4bfeea033a1cb2062fdfa2e
vrushabh <vrushabhkumatale1999@gmail.com>
2024-02-01 19:32:24 +0530

    Link Home and AddProduct Page in navbar

--- Product_Management_UI/product_ui/src/App.js
+++ Product_Management_UI/product_ui/src/App.js
@@ -1,10 +1,19 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './component/Navbar';
+import Home from './component/Home';
+import AddProduct from './component/AddProduct';
 
 function App() {
   return (
+    <>
     <Navbar/>
+<Routes>
+  <Route path='/' element={<Home/>}></Route>
+  <Route path='/addProduct' element={<AddProduct/>}></Route>
+</Routes>
+    </>
   );
 }
 
