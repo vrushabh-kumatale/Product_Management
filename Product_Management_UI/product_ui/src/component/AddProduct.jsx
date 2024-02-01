53b58fa70ebef4cd3cf92d319fbb199b35ff702e
vrushabh <vrushabhkumatale1999@gmail.com>
2024-02-01 21:59:49 +0530

    Add Product Bootstrap Complete and Submit Button

--- Product_Management_UI/product_ui/src/component/AddProduct.jsx
+++ Product_Management_UI/product_ui/src/component/AddProduct.jsx
@@ -2,7 +2,57 @@ import React from 'react';
 const AddProduct = () => {
     return (
         <>
-         <h1>Add Product Page</h1>
+         <div className="container mt-3">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <div className="card">
+                        <div className="card-header fs-3 text-center">
+                            Add Product
+                        </div>
+                        <div className="card-body">
+                            <form>
+                                <div className="mb-3">
+                                    <label>Enter Product Name</label>
+                                    <input
+                                       type="text"
+                                       name="productName"
+                                       className="form-control"
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label>Enter Description</label>
+                                    <input
+                                       type="text"
+                                       name="description"
+                                       className="form-control"
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label>Enter Price</label>
+                                    <input
+                                       type="text"
+                                       name="price"
+                                       className="form-control"
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label>Enter Status</label>
+                                    <input
+                                       type="text"
+                                       name="status"
+                                       className="form-control"
+                                    />
+                                </div>
+                                <button className="btn btn-primary col-md-12">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+         </div>
         </>
     )
 }
