e2fc4c422d569673f7203d42b337028ec40d9a35
vrushabh <vrushabhkumatale1999@gmail.com>
2024-02-01 18:43:37 +0530

    create AddProduct.js,EditProduct.js,Home.js

---
+++ Product_Management_UI/product_ui/src/component/AddProduct.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const AddProduct = () => {
+    return (
+        <>
+         <h1>Add Product Page</h1>
+        </>
+    )
+}
+
+export default AddProduct
---
+++ Product_Management_UI/product_ui/src/component/EditProduct.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const EditProduct = () => {
+    return (
+        <>
+         <h1>Edit Product Page</h1>
+        </>
+    )
+}
+
+export default EditProduct
